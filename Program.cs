cea312a34980ef3c04c4344525befe75c756595b
Alex_Mo <doj20@mail.ru>
2023-11-17 20:42:50 +0300

    First rev

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Please enter expresion as A+-/*B to get result.");
+string enter_str = Console.ReadLine();
+char [] expresion = enter_str.ToCharArray();
+
+if (expresion.Length == 3)
+{
+    int result = 0;
+    bool flag = false;
+    int operand_1 = Convert.ToInt16(expresion[0].ToString());
+    char operation = expresion[1];
+    int operand_2 = Convert.ToInt16(expresion[2].ToString());
+
+    switch (operation)
+    {
+        case '-':
+            result = operand_1 - operand_2;
+            flag = true;
+            break;
+
+        case '+':
+            result = operand_1 + operand_2;
+            flag = true;
+            break;
+
+        case '/':
+            result = operand_1 / operand_2;
+            flag = true;
+            break;
+
+        case '*':
+            result = operand_1 * operand_2;
+            flag = true;
+            break;
+
+        default:
+            Console.WriteLine("Wrong expresion:" + " " + operation + ". " + "Try again!");
+            break;
+    }
+    if (flag) 
+    {
+        Console.WriteLine(operand_1 + " " + operation + " " + operand_2 + " =" + " " + result);
+    } 
+}
+
